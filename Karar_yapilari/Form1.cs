28a30e5c62244949d81b7603040647fef5f57261
Enis Akturk <67652635+enisakturk@users.noreply.github.com>
2021-10-17 15:11:57 +0300

    Add project files.

---
+++ Karar_yapilari/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Karar_yapilari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double s1, s2, project, avg;
+            string state;
+            s1=Convert.ToInt16(textBox1.Text);
+            s2 = Convert.ToInt16(textBox2.Text);
+            project = Convert.ToInt16(textBox3.Text);
+            avg = (s1 + s2 + project) / 3;
+
+            if (avg>=50)
+            {
+                state=("Passed the lesson");
+            }
+            else
+            {
+                state=("didn't pass the lesson");
+            }
+            textBox4.Text = avg + "/ " + state;
+
+        }
+    }
+}
